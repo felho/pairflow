fa5de1e4e6a7407363c7dcd45ac5433b8bd98b41
Gergely Hodicska <gergely.hodicska@gmail.com>
2026-03-19 23:05:44 +0100

    refactor(resume): add v11 command seams and orchestration

---
+++ src/v11/shared/resume/resumeCommandOrchestration.ts
@@ -0,0 +1,22 @@
+import { emitHumanReply } from "../../../core/human/reply.js";
+import {
+  DEFAULT_RESUME_MESSAGE,
+  type ResumeBubbleDependencies,
+  type ResumeBubbleInput,
+  type ResumeBubbleResult
+} from "../../application/resume/resumeCommandContract.js";
+
+export async function resumeBubbleCommandOrchestration(
+  input: ResumeBubbleInput,
+  dependencies: ResumeBubbleDependencies = {}
+): Promise<ResumeBubbleResult> {
+  const emitReply = dependencies.emitHumanReply ?? emitHumanReply;
+
+  return emitReply({
+    bubbleId: input.bubbleId,
+    message: DEFAULT_RESUME_MESSAGE,
+    ...(input.repoPath !== undefined ? { repoPath: input.repoPath } : {}),
+    ...(input.cwd !== undefined ? { cwd: input.cwd } : {}),
+    ...(input.now !== undefined ? { now: input.now } : {})
+  });
+}
