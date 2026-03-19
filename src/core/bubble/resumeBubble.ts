b1af6a42e14b74235ef868a0d216092c02109236
Gergely Hodicska <gergely.hodicska@gmail.com>
2026-03-19 23:06:35 +0100

    refactor(resume): switch core entrypoint to v11 facade

--- src/core/bubble/resumeBubble.ts
+++ src/core/bubble/resumeBubble.ts
@@ -1,64 +1,11 @@
-import {
-  asHumanReplyCommandError,
-  emitHumanReply,
-  type EmitHumanReplyResult
-} from "../human/reply.js";
-
-export const DEFAULT_RESUME_MESSAGE =
-  "Operator resumed ping-pong after manual intervention.";
-
-export interface ResumeBubbleInput {
-  bubbleId: string;
-  repoPath?: string | undefined;
-  cwd?: string | undefined;
-  now?: Date | undefined;
-}
-
-export type ResumeBubbleResult = EmitHumanReplyResult;
-
-export interface ResumeBubbleDependencies {
-  emitHumanReply?: typeof emitHumanReply;
-}
-
-export class ResumeBubbleError extends Error {
-  public constructor(message: string) {
-    super(message);
-    this.name = "ResumeBubbleError";
-  }
-}
-
-export async function resumeBubble(
-  input: ResumeBubbleInput,
-  dependencies: ResumeBubbleDependencies = {}
-): Promise<ResumeBubbleResult> {
-  const emitReply = dependencies.emitHumanReply ?? emitHumanReply;
-
-  try {
-    return await emitReply({
-      bubbleId: input.bubbleId,
-      message: DEFAULT_RESUME_MESSAGE,
-      ...(input.repoPath !== undefined ? { repoPath: input.repoPath } : {}),
-      ...(input.cwd !== undefined ? { cwd: input.cwd } : {}),
-      ...(input.now !== undefined ? { now: input.now } : {})
-    });
-  } catch (error) {
-    asResumeBubbleError(error);
-  }
-}
-
-export function asResumeBubbleError(error: unknown): never {
-  if (error instanceof ResumeBubbleError) {
-    throw error;
-  }
-
-  try {
-    asHumanReplyCommandError(error);
-  } catch (humanReplyError) {
-    if (humanReplyError instanceof Error) {
-      throw new ResumeBubbleError(humanReplyError.message);
-    }
-    throw humanReplyError;
-  }
-
-  throw error;
-}
+export {
+  asResumeBubbleErrorV11 as asResumeBubbleError,
+  DEFAULT_RESUME_MESSAGE,
+  ResumeBubbleErrorV11 as ResumeBubbleError,
+  resumeBubbleV11 as resumeBubble
+} from "../../v11/application/resume/emitResumeV11.js";
+export type {
+  ResumeBubbleV11Dependencies as ResumeBubbleDependencies,
+  ResumeBubbleV11Input as ResumeBubbleInput,
+  ResumeBubbleV11Result as ResumeBubbleResult
+} from "../../v11/application/resume/emitResumeV11.js";
