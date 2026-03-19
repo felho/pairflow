9e4026c57b2e8e8960bffec392f52b7793500559
Gergely Hodicska <gergely.hodicska@gmail.com>
2026-03-19 23:25:08 +0100

    refactor(watchdog): migrate command flow to v11 api

---
+++ src/v11/application/watchdog/emitWatchdogV11.ts
@@ -0,0 +1,11 @@
+export {
+  asBubbleWatchdogError as asBubbleWatchdogErrorV11,
+  BubbleWatchdogError as BubbleWatchdogErrorV11,
+  runBubbleWatchdog as runBubbleWatchdogV11
+} from "../../shared/watchdog/watchdogCommandApi.js";
+export type {
+  BubbleWatchdogDependencies as BubbleWatchdogV11Dependencies,
+  BubbleWatchdogInput as BubbleWatchdogV11Input,
+  BubbleWatchdogNoopReason as BubbleWatchdogV11NoopReason,
+  BubbleWatchdogResult as BubbleWatchdogV11Result
+} from "./watchdogCommandContract.js";
