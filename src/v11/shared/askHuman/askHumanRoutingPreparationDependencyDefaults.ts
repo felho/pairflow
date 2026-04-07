81690871a76533321fb153b8c888a20405d74864
Gergely Hodicska <gergely.hodicska@gmail.com>
2026-04-07 18:07:43 +0200

    refactor(v11): move askHuman routing prep into application

--- src/v11/shared/askHuman/askHumanRoutingPreparationDependencyDefaults.ts
+++
@@ -1,9 +0,0 @@
-import { ensureBubbleInstanceIdForMutation } from "../../../core/bubble/bubbleInstanceId.js";
-import { resolveBubbleFromWorkspaceCwd } from "../../../core/bubble/workspaceResolution.js";
-import { readStateSnapshot } from "../../../core/state/stateStore.js";
-
-export const askHumanRoutingPreparationDependencyDefaults = {
-  resolveBubbleFromWorkspaceCwd,
-  ensureBubbleInstanceIdForMutation,
-  readStateSnapshot
-} as const;
