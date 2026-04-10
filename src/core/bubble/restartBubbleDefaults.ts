befb3f942c21e9c6517673de8e95037605f8784b
Gergely Hodicska <gergely.hodicska@gmail.com>
2026-04-10 08:12:49 +0200

    refactor(core-zero): delete restart defaults bridge

--- src/core/bubble/restartBubbleDefaults.ts
+++
@@ -1,6 +0,0 @@
-import { restartBubbleDependencyDefaults as restartBubbleDefaultsV11 } from "../../v11/infrastructure/executor/restart/restartCommandDefaults.js";
-
-export type RestartBubbleDefaultDependencies = typeof restartBubbleDefaultsV11;
-
-export const restartBubbleDependencyDefaults: RestartBubbleDefaultDependencies =
-  restartBubbleDefaultsV11;
--- src/v11/defaults/restart/restartCommandDefaults.ts
+++ src/v11/defaults/restart/restartCommandDefaults.ts
@@ -1,22 +1,17 @@
+import {
+  restartBubbleDependencyDefaults as restartBubbleDefaultsCanonical
+} from "../../infrastructure/executor/restart/restartCommandDefaults.js";
 import type { ResolveBubbleByIdPort } from "../../shared/ports/bubbleLookup.js";
 import type { PersistPassValidationRecoveryMarkerPort } from "../../shared/ports/passValidationRecovery.js";
 import type { RemoveRuntimeSessionPort } from "../../shared/ports/runtimeSessions.js";
 import type { TerminateBubbleTmuxSessionPort } from "../../shared/ports/tmuxSessions.js";
 
-export interface RestartBubbleDefaultDependencies {
+interface RestartBubbleDefaultDependencies {
   resolveBubbleById: ResolveBubbleByIdPort;
   terminateBubbleTmuxSession: TerminateBubbleTmuxSessionPort;
   removeRuntimeSession: RemoveRuntimeSessionPort;
   persistPassValidationRecoveryMarker: PersistPassValidationRecoveryMarkerPort;
 }
 
-let restartBubbleDependencyDefaultsPromise:
-  | Promise<RestartBubbleDefaultDependencies>
-  | undefined;
-
-export async function loadRestartBubbleDependencyDefaults(): Promise<RestartBubbleDefaultDependencies> {
-  restartBubbleDependencyDefaultsPromise ??= import(
-    "../../defaults/restart/restartCommandDefaults.js"
-  ).then(({ restartBubbleDependencyDefaults }) => restartBubbleDependencyDefaults);
-  return restartBubbleDependencyDefaultsPromise;
-}
+export const restartBubbleDependencyDefaults: RestartBubbleDefaultDependencies =
+  restartBubbleDefaultsCanonical;
