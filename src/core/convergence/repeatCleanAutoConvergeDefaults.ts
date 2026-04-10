4c9301aa051557a0267f91d7295a5b4b392db490
Gergely Hodicska <gergely.hodicska@gmail.com>
2026-04-10 08:39:20 +0200

    refactor(core-zero): delete unused core bridges

--- src/core/convergence/repeatCleanAutoConvergeDefaults.ts
+++
@@ -1,7 +0,0 @@
-import { readStateSnapshot } from "../state/stateStore.js";
-import { writeReviewVerificationArtifactAtomic } from "../../v11/shared/reviewer/reviewVerificationArtifactReaders.js";
-
-export const repeatCleanAutoConvergeDefaults = {
-  readStateSnapshot,
-  writeReviewVerificationArtifactAtomic
-} as const;
