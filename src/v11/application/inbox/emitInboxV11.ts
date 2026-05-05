a570376bbf899e2e6a77eb326b8e480dcc67f56e
Gergely Hodicska <gergely.hodicska@gmail.com>
2026-05-05 03:54:44 +0200

    bubble(5-inbox-api-rename-impl): finalize

--- src/v11/application/inbox/emitInboxV11.ts
+++ src/v11/application/inbox/emitInboxV11.ts
@@ -2,10 +2,10 @@ export {
   asBubbleInboxError as asBubbleInboxErrorV11,
   BubbleInboxError as BubbleInboxErrorV11,
   getBubbleInbox as getBubbleInboxV11
-} from "../../shared/inbox/inboxCommandApi.js";
+} from "../../shared/bubbleInbox/bubbleInboxReadModel.js";
 export type {
   BubbleInboxInput as BubbleInboxV11Input,
   BubbleInboxView as BubbleInboxV11View,
   PendingInboxItem as PendingInboxItemV11,
   PendingInboxItemType as PendingInboxItemV11Type
-} from "../../shared/inbox/inboxCommandApi.js";
+} from "../../shared/bubbleInbox/bubbleInboxReadModel.js";
