1992c28fd3174f62e76daebbbf97f509d96ac54b
Gergely Hodicska <gergely.hodicska@gmail.com>
2026-05-06 23:17:26 +0200

    chore: remove stale reviewer snapshot type import

--- src/v11/shared/metaReviewGate/internal/metaReviewGateReviewerSnapshot.ts
+++ src/v11/shared/metaReviewGate/internal/metaReviewGateReviewerSnapshot.ts
@@ -1,7 +1,3 @@
-import {
-  type LatestSameRoundReviewerSnapshot
-} from "../../../domain/metaReviewGate/reviewerSnapshot.js";
-
 export {
   isAdvisoryOnlyReviewerSnapshot,
   isReviewerSnapshotEnvelope,
