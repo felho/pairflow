ffa6ae157e651474a8c909cc687bacfd13fa5b39
Gergely Hodicska <gergely.hodicska@gmail.com>
2026-05-07 01:09:41 +0200

    fitness: remove shared converged defaults facade

--- src/v11/shared/converged/convergedDependencyDefaults.ts
+++
@@ -1,5 +0,0 @@
-const convergedDependencyDefaultsPromise = import(
-  "../../defaults/converged/convergedDependencyDefaults.js"
-).then(({ convergedDependencyDefaults }) => convergedDependencyDefaults);
-
-export const convergedDependencyDefaults = await convergedDependencyDefaultsPromise;
